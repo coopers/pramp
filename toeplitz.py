0978b5231f4276f60649abe9274868a98089046a
Chris Cooper <christopher.p.cooper@gmail.com>
2023-12-15 19:40:03 -0800

    refactor

--- toeplitz.py
+++ toeplitz.py
@@ -1,3 +1,7 @@
 def isToeplitz(arr):
-    R, C = len(arr), len(arr[0])
-    return all(arr[r][c] == arr[r-1][c-1] for r in range(1, R) for c in range(1, C))
+    ROWS, COLS = len(arr), len(arr[0])
+    return all(
+        arr[r][c] == arr[r-1][c-1]
+        for r in range(1, ROWS)
+        for c in range(1, COLS)
+    )
