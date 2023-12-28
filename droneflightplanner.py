5b788a10fff8c9e243d53a41f8c241c4058ceb27
Chris Cooper <christopher.p.cooper@gmail.com>
2023-12-28 08:31:11 -0800

    refactor

--- droneflightplanner.py
+++ droneflightplanner.py
@@ -1,14 +1,12 @@
 def calc_drone_min_energy(route):
-    start = route[0][-1]
-    maximum = max(route[i][-1] for i in range(len(route)))
-    return max(0, maximum - start)
+    return max(r[-1] for r in route) - route[0][-1]
 
 
 def calc_drone_min_energy(route):
-  energy = 0
-  needed = 0
-  for i in range(1, len(route)):
-    energy += route[i-1][-1] - route[i][-1]
-    needed = min(needed, energy)
-        
-  return -needed
+    energy = 0
+    needed = 0
+    for i in range(1, len(route)):
+        energy += route[i-1][-1] - route[i][-1]
+        needed = min(needed, energy)
+    
+    return -needed
--- findtheduplicates.py
+++ findtheduplicates.py
@@ -1,26 +1,29 @@
 import bisect
+import math
 
 def find_duplicates(lil, big):
-  res = []
-  if len(lil) > len(big):
-    lil, big = big, lil
+    res = []
+    if len(lil) > len(big):
+        lil, big = big, lil
 
-  L, B = len(lil), len(big)
-  if B >= 2 ** L:
-    for num in lil:
-      i = bisect.bisect_left(big, num)
-      if i < B and big[i] == num:
-        res.append(num)
-  else:
-    l = b = 0
-    while l < L and b < B:
-      if lil[l] == big[b]:
-        res.append(lil[l])
-        l += 1
-        b += 1
-      elif lil[l] < big[b]:
-        l += 1
-      else:
-        b += 1
-  
-  return res
+    L, B = len(lil), len(big)
+    if L * math.log(B) / math.log(2) < L + B:
+        for num in lil:
+            i = bisect.bisect_left(big, num)
+            if i == B:
+                break
+            if big[i] == num:
+                res.append(num)
+    else:
+        l = b = 0
+        while l < L and b < B:
+            if lil[l] == big[b]:
+                res.append(lil[l])
+                l += 1
+                b += 1
+            elif lil[l] < big[b]:
+                l += 1
+            else:
+                b += 1
+
+    return res
