bcca95d868f7342da88f400de067e5ebd07d6442
Chris Cooper <christopher.p.cooper@gmail.com>
2024-01-03 13:49:55 -0800

    refactor

--- get_shortest_unique_substring.py
+++ get_shortest_unique_substring.py
@@ -1,20 +1,20 @@
 def get_shortest_unique_substring(arr, s):
   counts = {n: 0 for n in arr}
-  needed = len(arr)
+  need = len(arr)
   l = 0
   res = ''
   for r in range(len(s)):
     if s[r] in counts:
       if not counts[s[r]]:
-        needed -= 1
+        need -= 1
       counts[s[r]] += 1
-      while not needed:
+      while not need:
         if not res or r - l < len(res):
           res = s[l:r+1]
         if s[l] in counts:
           counts[s[l]] -= 1
           if not counts[s[l]]:
-            needed += 1
+            need += 1
         l += 1
         
   return res
