4033e2e0f85e1d1aa4895cc73c920d9e0507a855
Chris Cooper <christopher.p.cooper@gmail.com>
2024-01-07 15:29:52 -0800

    bst_successor_search.py

---
+++ bst_successor_search.py
@@ -0,0 +1,117 @@
+#########################################################
+# CODE INSTRUCTIONS:                                    #
+# 1) The method findInOrderSuccessor you're asked      #
+#    to implement is located at line 30.                #
+# 2) Use the helper code below to implement it.         #
+# 3) In a nutshell, the helper code allows you to       #
+#    to build a Binary Search Tree.                     #
+# 4) Jump to line 88 to see an example for how the      #
+#    helper code is used to test findInOrderSuccessor.  #
+#########################################################
+
+
+# A node 
+class Node:
+
+  # Constructor to create a new node
+  def __init__(self, key):
+    self.key = key 
+    self.left = None
+    self.right = None
+    self.parent = None
+
+# A binary search tree 
+class BinarySearchTree:
+
+  # Constructor to create a new BST
+  def __init__(self):
+    self.root = None 
+
+  def find_in_order_successor(self, node):
+    if not node:
+      return None
+    
+    if node.right:
+      node = node.right
+      while node.left:
+        node = node.left
+      return node
+    
+    key = node.key
+    while node.parent:
+      node = node.parent
+      if node.key > key:
+        return node
+      
+    return None
+
+
+
+  # Given a binary search tree and a number, inserts a
+  # new node with the given number in the correct place
+  # in the tree. Returns the new root pointer which the
+  # caller should then use(the standard trick to avoid 
+  # using reference parameters)
+  def insert(self, key):
+    
+    # 1) If tree is empty, create the root
+    if (self.root is None):
+      self.root = Node(key)
+      return
+        
+    # 2) Otherwise, create a node with the key
+    #    and traverse down the tree to find where to
+    #    to insert the new node
+    currentNode = self.root
+    newNode = Node(key)
+    while(currentNode is not None):
+      
+      if(key < currentNode.key):
+        if(currentNode.left is None):
+          currentNode.left = newNode;
+          newNode.parent = currentNode;
+          break
+        else:
+          currentNode = currentNode.left;
+      else:
+        if(currentNode.right is None):
+          currentNode.right = newNode;
+          newNode.parent = currentNode;
+          break
+        else:
+          currentNode = currentNode.right;
+
+  # Return a reference to a node in the BST by its key.
+  # Use this method when you need a node to test your
+  # findInOrderSuccessor function on
+  def getNodeByKey(self, key):
+    
+    currentNode = self.root
+    while(currentNode is not None):
+      if(key == currentNode.key):
+        return currentNode
+        
+      if(key < currentNode.key):
+        currentNode = currentNode.left
+      else:
+        currentNode = currentNode.right
+        
+    return None
+        
+######################################### 
+# Driver program to test above function #
+#########################################
+
+# Create a Binary Search Tree
+bst  = BinarySearchTree()
+keys = [5, 9, 11, 12, 14, 20, 25]
+for k in keys:
+    bst.insert(k)  
+
+for i in range(len(keys)-1):
+  node = bst.getNodeByKey(keys[i])
+  successor = bst.getNodeByKey(keys[i+1])
+  assert bst.find_in_order_successor(node) == successor
+
+last_node = bst.getNodeByKey(keys[-1])
+assert bst.find_in_order_successor(last_node) == None
