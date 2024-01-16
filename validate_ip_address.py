cf05362f30cbc975d496a848eb09bb14cf2e0d15
Chris Cooper <christopher.p.cooper@gmail.com>
2024-01-15 22:57:40 -0800

    validate does not accept leading zeros, like 010

--- validate_ip_address.py
+++ validate_ip_address.py
@@ -7,6 +7,7 @@ def validateIP(ip):
         return False
 
     return all(
+        s != '' and
         (len(s) == 1 or s[0] != '0') and
         s.isdigit() and
         0 <= int(s) <= 255
@@ -18,3 +19,4 @@ assert validateIP('0.0.0.0') == True
 assert validateIP('123.24.59.99') == True
 assert validateIP('192.168.123.456') == False
 assert validateIP('0.00.0.0') == False
+assert validateIP('.00.0.0') == False
