694ff7bb3e093ecebf83a555f273adf1287351b1
Chris Cooper <christopher.p.cooper@gmail.com>
2024-02-10 17:02:17 -0800

    used bfs

--- shortest_word_edit_path.py
+++ shortest_word_edit_path.py
@@ -1,47 +1,35 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 
 def get_transforms(word):
     return ["%s*%s" % (word[:i], word[i + 1:]) for i in range(len(word))]
 
 def shortestWordEditPath(source, target, words):
-    words = set(words)
-    if target not in words:
+    remaining = set(words)
+    if target not in remaining:
         return -1
 
     transformToWords = defaultdict(set)
-    for word in words:
+    for word in remaining:
         for transform in get_transforms(word):
             transformToWords[transform].add(word)
 
     count = 0
-    words = set([source])
+    words = deque([source])
     while words:
-        if target in words:
-            return count
+        for _ in range(len(words)):
+            word = words.popleft()
+            if word == target:
+                return count
+
+            for transform in get_transforms(word):
+                if transform in transformToWords:
+                    for next_word in transformToWords[transform]:
+                        if next_word in remaining:
+                            words.append(next_word)
+                            remaining.remove(next_word)
+                    
+                    del transformToWords[transform]
 
         count += 1
-        transforms = {transform
-                        for word in words
-                        for transform in get_transforms(word)
-                        if transform in transformToWords}
-
-        words = {word
-                    for transform in transforms
-                    for word in transformToWords[transform]}
-
-        for transform in transforms:
-            del transformToWords[transform]
 
     return -1
-
-source = "bit"
-target = "dog"
-words = ["but", "put", "big", "pot", "pog", "dog", "lot"]
-output = 5
-assert shortestWordEditPath(source, target, words) == output
-
-source = "no"
-target = "go"
-words = ["to"]
-output = -1
-assert shortestWordEditPath(source, target, words) == output
