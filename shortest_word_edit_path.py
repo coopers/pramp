71cfb1e5f5c50828944d4fe6bbdf414ae984a6f7
Chris Cooper <christopher.p.cooper@gmail.com>
2024-02-10 16:33:20 -0800

    refactor

--- shortest_word_edit_path.py
+++ shortest_word_edit_path.py
@@ -1,39 +1,38 @@
 from collections import defaultdict
 
 def get_transforms(word):
-  return ["%s*%s" % (word[:i], word[i + 1:]) for i in range(len(word))]
+    return ["%s*%s" % (word[:i], word[i + 1:]) for i in range(len(word))]
 
 def shortestWordEditPath(source, target, words):
-  words = set(words)
-  if target not in words:
+    words = set(words)
+    if target not in words:
+        return -1
+
+    transformToWords = defaultdict(set)
+    for word in words:
+        for transform in get_transforms(word):
+            transformToWords[transform].add(word)
+
+    count = 0
+    words = set([source])
+    while words:
+        if target in words:
+            return count
+
+        count += 1
+        transforms = {transform
+                        for word in words
+                        for transform in get_transforms(word)
+                        if transform in transformToWords}
+
+        words = {word
+                    for transform in transforms
+                    for word in transformToWords[transform]}
+
+        for transform in transforms:
+            del transformToWords[transform]
+
     return -1
-  
-  transformToWords = defaultdict(set)
-  for word in words:
-    for transform in get_transforms(word):
-      transformToWords[transform].add(word)
-
-  count = 0
-  words = set()
-  words.add(source)
-  while words:
-    if target in words:
-      return count
-    
-    count += 1
-    transforms = {transform
-                    for word in words
-                    for transform in get_transforms(word)
-                    if transform in transformToWords}
-    
-    words = {word
-                for transform in transforms
-                for word in transformToWords[transform]}
-    
-    for transform in transforms:
-      del transformToWords[transform]
-
-  return -1
 
 source = "bit"
 target = "dog"
