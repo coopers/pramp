8134866814caf6a10d2e29ddb72d2b6286353cd9
Chris Cooper <christopher.p.cooper@gmail.com>
2024-02-19 15:16:02 -0800

    refactor

--- decode_variations.py
+++ decode_variations.py
@@ -3,14 +3,10 @@ def decodeVariations(S):
     cur = one = two = 1
     for i in reversed(range(len(S))):
         ch = S[i]
-        if ch == '0':
-            cur = 0
-        else:
-            cur = one
-            if pre is not None and (ch == '1' or (ch == '2' and '0' <= pre <= '6')):
-                cur += two
+        cur = 0 if ch == '0' else one
+        if pre is not None and (ch == '1' or (ch == '2' and '0' <= pre <= '6')):
+            cur += two
 
-        pre = ch
-        one, two = cur, one
+        one, two, pre = cur, one, ch
 
     return one
