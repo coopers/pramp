a592028bc3d585fff92cf53449c78eb77c2e5bb1
Chris Cooper <christopher.p.cooper@gmail.com>
2024-02-26 13:55:01 -0800

    refactor

--- number_of_paths.py
+++ number_of_paths.py
@@ -1,8 +1,8 @@
 def num_of_paths_to_dest(n):
     dp = [1] * n
-    for i in range(1, n-1):
-        for j in range(i+1, n):
-            dp[j] += dp[j-1]
+    for i in range(1, n - 1):
+        for j in range(i + 1, n):
+            dp[j] += dp[j - 1]
 
     return dp[-1]
 
